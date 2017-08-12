b954c16f6777ebeda8b15846989cd926a0548102
justinvos <justinvosnz@gmail.com>
2017-08-12 14:22:04 +1200

    Added typescript support

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,19 @@
 var gulp = require("gulp")
 var sass = require("gulp-sass")
+var ts = require('gulp-typescript')
 
 gulp.task("sass", [], function() {
-  gulp.src('client/styles/main.sass')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('public'));
+  gulp.src("client/styles/main.sass")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest("public"))
+})
+
+gulp.task("ts", [], function() {
+  gulp.src("server/**/*.ts")
+    .pipe(ts({
+            moduleResolution: "node",
+            noImplicitAny: true,
+            outFile: 'server.js'
+        }))
+    .pipe(gulp.dest("dist"))
 })
